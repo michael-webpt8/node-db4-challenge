90d96d29095d4712bf2645aedb31cc2a46c58830
Michael Williams <manicdesigner@gmail.com>
2020-01-09 13:29:47 -0500

    fix recipe ingredient

--- seeds/003_recipe_ingredients.js
+++ seeds/003_recipe_ingredients.js
@@ -4,5 +4,6 @@ exports.seed = async function(knex) {
     { recipe_id: 1, ingredient_id: 2 },
     { recipe_id: 2, ingredient_id: 3 },
     { recipe_id: 2, ingredient_id: 4 },
+    { recipe_id: 3, ingredient_id: 5 },
   ]);
 };
