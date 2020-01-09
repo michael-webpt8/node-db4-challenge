6a718ced8477171dbe0555c88a82755087128adb
Michael Williams <manicdesigner@gmail.com>
2020-01-09 13:30:21 -0500

    add recipes

--- seeds/001_recipes.js
+++ seeds/001_recipes.js
@@ -2,5 +2,6 @@ exports.seed = async function(knex) {
   await knex('recipes').insert([
     { recipe_name: 'toast' },
     { recipe_name: 'cereal' },
+    { recipe_name: 'french toast' },
   ]);
 };
