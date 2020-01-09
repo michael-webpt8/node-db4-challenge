6665c902f480fd324077db7fa2714c1d7979dbe2
Michael Williams <manicdesigner@gmail.com>
2020-01-09 10:02:05 -0500

    created recipes, ingredients recipes_ingredients tables

--- migrations/20200109090202_initial.js
+++ migrations/20200109090202_initial.js
@@ -1,9 +1,31 @@
 exports.up = async function(knex) {
-  await knex.schema.createTable('recipes', table => {});
+  await knex.schema.createTable('recipes', table => {
+    table.increments('id');
+    table.string('recipe_name', 250).notNullable();
+  });
 
-  await knex.schema.createTable('ingredients', table => {});
+  await knex.schema.createTable('ingredients', table => {
+    table.increments('id');
+    table.string('ingredient', 160).notNullable();
+    table.float('quantity').notNullable();
+    table.string('size').notNullable();
+  });
 
-  await knex.schema.createTable('recipes_ingreidents', table => {});
+  await knex.schema.createTable('recipes_ingreidents', table => {
+    table
+      .integer('recipe_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('recipes');
+    table
+      .integer('ingredient_id')
+      .unsigned()
+      .notNullable()
+      .references('id')
+      .inTable('ingredients');
+    table.primary(['recipe_id', 'ingredient_id']);
+  });
 
   await knex.schema.createTable('recipe_instructions', table => {});
 };
