36ce3b7b7002bc16e4cab6021a8d42d5d452aa05
Mikey Williams <manicdesigner@gmail.com>
2020-01-10 18:00:55 -0500

    mod variable to normal string representation

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -13,7 +13,7 @@ function getRecipes() {
 function getShoppingList(recipe_id) {
   return db('recipes_ingredients as ri')
     .join('ingredients as i', 'ri.ingredient_id', 'i.id')
-    .join('recipes as r', 'ri.recipe_id', recipe_id)
+    .join('recipes as r', 'ri.recipe_id', 'r.id')
     .where({ recipe_id });
 }
 
