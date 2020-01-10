32140fe9f2a946374978b7c49a238b21f1387367
Mikey Williams <manicdesigner@gmail.com>
2020-01-10 18:00:31 -0500

    add server file

--- server.js
+++ server.js
@@ -3,11 +3,20 @@ const cors = require('cors');
 const helmet = require('helmet');
 const recipesRouter = require('./recipes/recipes-router');
 
-const server = express();
-server.use(helmet());
-server.use(cors());
-server.use(express.json());
+const router = express();
+router.use(helmet());
+router.use(cors());
+router.use(express.json());
 
-server.use('/api/recipes', recipesRouter);
+router.use('/api/recipes', recipesRouter);
+
+router.use('/', (req, res) => {
+    res.send('api is up');
+})
+
+router.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({ errorMessage: "server error, sorry try again later." })
+})
 
 module.exports = router;
