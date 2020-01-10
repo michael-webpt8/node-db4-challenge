04d976464e6093ca4da413754afa384864340fb9
Michael Williams <manicdesigner@gmail.com>
2020-01-09 20:06:53 -0500

    add an ingredient rm quantity

--- seeds/002_ingredients.js
+++ seeds/002_ingredients.js
@@ -1,9 +1,10 @@
 exports.seed = async function(knex) {
   await knex('ingredients').insert([
-    { ingredient: 'bread', quantity: 2 },
-    { ingredient: 'butter', quantity: 1, size: 'tbp' },
-    { ingredient: 'milk', quantity: 1, size: 'pint' },
-    { ingredient: 'cereal', quantity: 1, size: 'bowl' },
-    { ingredient: 'cinnamon', quantity: 0.75, size: 'cup' },
+    { ingredient: 'bread' },
+    { ingredient: 'butter' },
+    { ingredient: 'milk' },
+    { ingredient: 'cereal' },
+    { ingredient: 'cinnamon' },
+    { ingredient: 'syrup' },
   ]);
 };
