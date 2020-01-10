2097aebda8e75de11c732d90feed7165d5b91569
Mikey Williams <manicdesigner@gmail.com>
2020-01-10 18:11:25 -0500

    selected certain items in getShoppingList

--- recipes/recipes-model.js
+++ recipes/recipes-model.js
@@ -14,7 +14,7 @@ function getShoppingList(recipe_id) {
   return db('recipes_ingredients as ri')
     .join('ingredients as i', 'ri.ingredient_id', 'i.id')
     .join('recipes as r', 'ri.recipe_id', 'r.id')
-    .where({ recipe_id });
+    .where({ recipe_id }).select('recipe_name', 'ingredient', 'quantity');
 }
 
 function getInstructions(recipe_id) {
